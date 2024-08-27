a5404f28bf637a6871c4b30707b7c30d671055a5
megait <megait@gmail.com>
2024-08-27 21:21:33 +0900

    [정주원 학원] - lombok 환경설정 및 개발환경 설정

---
+++ src/main/java/com/joowon/spring/ex/jpa/StudentController.java
@@ -0,0 +1,34 @@
+package com.joowon.spring.ex.jpa;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.joowon.spring.ex.jpa.domain.Student;
+
+@Controller
+public class StudentController {
+
+	// C, U, D
+	
+	
+	
+	@GetMapping("/jpa/lombok/test")
+	@ResponseBody
+	public Student lombokTest(){
+		
+//		Student student = new Student(10, "정주원", "010-1111-2222", "jung@naver.com", "프로그래머", null, null);
+//		student.setPhoneNumber("010-9999-0000");
+		
+		Student student = Student.builder()
+					.name("정주원")
+					.phoneNumber("010-0000-1111")
+					.email("jung@naver.com")
+					.dreamJob("프로그래머")
+					.build();
+		
+		return student;
+	}
+	
+	
+}
---
+++ src/main/java/com/joowon/spring/ex/jpa/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.joowon.spring.ex.jpa.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+	
+	public addStudent(String name, String phoneNumber, String email, String dreamJob){
+		
+	}
+}
